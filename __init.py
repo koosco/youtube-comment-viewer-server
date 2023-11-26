f9ad2a4f9496b8c3b2686dac38e24540df998f49
koosco <rnxogud123@naver.com>
2023-11-27 02:05:20 +0900

    CORS error 수정
    flask_cors를 추가하여 CORS 에러를 방지
    resources를 임시적으로 모두 허용함

--- __init.py
+++ __init.py
@@ -8,33 +8,35 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={
+  r"*": {"origin": "*"}
+})
 
 @app.route('/post', methods=['POST'])
 def post_response():
     driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()))
-    url = request.form.get('url')
-    print('received url is', url)
+    start = time.time()
+    # url = request.form.get('url')
+    data = request.get_json()
+    if data and 'url' in data:
+        url = data['url']
+    print(url)
+    print(type(url))
 
-    print('check point: scraping start')
     driver.get(url)
     start = time.time()
     time.sleep(SCROLL_PAUSE_TIME)
-    print('check point: before get page')
     actions = driver.find_element(By.CSS_SELECTOR, 'body')
 
-    print('check point: before Keys.END')
-    print('time:', time.time() - start)
-    for i in range(1, 11):
+    for i in range(1, 6):
         #driver.execute_script("window.scrollTo(0, document.body.scrollHeight, 'instant');")
         actions.send_keys(Keys.END)
         time.sleep(1)
-        print('check point: scroll loop', i)
 
-    print('check point: before get page source')
-    print('time:', time.time() - start)
-    time.sleep(3)
+    time.sleep(2)
     html_source = driver.page_source
     soup = BeautifulSoup(html_source, 'html.parser')
 
@@ -42,19 +44,17 @@ def post_response():
     comment_final = []
 
     end = time.time()
-    print('총 소요 시간:', end - start)
-    print('check point: remove \\n, \\t')
     for i in range(len(comment_list)):
         temp_comment = comment_list[i].text
         temp_comment = temp_comment.replace('\n', '')
         temp_comment = temp_comment.replace('\t', '')
         comment_final.append(temp_comment)
 
-    print('program still working')
-    result = '\n'.join(comment_final)
+    result = ' '.join(comment_final)
     driver.close()
     
-
+    end = time.time()
+    print(end - start)
     data = {'text': result}
     with open('res.txt', 'w') as f:
         for comment in comment_final:
@@ -64,6 +64,6 @@ def post_response():
 
 if __name__ == '__main__':
     SCROLL_PAUSE_TIME = 0.7
-    display = Display(visible=0, size=(1024, 768))
-    display.start()
-    app.run('0.0.0.0', port=5000, debug=True)
+    # display = Display(visible=0, size=(1024, 768))
+    # display.start()
+    app.run('0.0.0.0', port=8081, debug=True)
